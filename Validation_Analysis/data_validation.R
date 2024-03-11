fc017507404084dc3df977466d16d9b7cd9f004c
Anand7Choudhary <raschoudhary879@gmail.com>
2024-03-11 16:45:06 +0000

    Workflow check

---
+++ Validation_Analysis/data_validation.R
@@ -0,0 +1,18 @@
+# Define the path to the README file
+readme_path <- "../README.md"
+
+# Read the current content of the README
+readme_content <- readLines(readme_path, warn = FALSE)
+
+# Get the current timestamp
+timestamp <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+
+# Define the message to append, including the name of the script
+script_name <- "data_val" # Replace with your actual script name
+update_message <- paste0("- ", script_name, " last run at ", timestamp)
+
+# Append the update message to the README content
+readme_content <- c(readme_content, update_message)
+
+# Write the updated content back to the README
+writeLines(readme_content, readme_path)
